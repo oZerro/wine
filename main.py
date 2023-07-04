1d6646be9f713e3c27c746bb9c85305e6ecfcf61
oZerro <leha.normatov@mail.ru>
2023-07-04 18:27:13 +0300

    Changed the name of the function

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 
-def declension(age, for_1, for_2, for_other):
+def get_declination_year(age, for_1, for_2, for_other):
     residue = age % 10
 
     if residue == 1 and age % 100 != 11:
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     categories_wine = get_all_wine_info('wine3.xlsx')
     rendered_page = template.render(
         categories_wine=categories_wine,
-        winery_age=declension(winery_age, "год", "года", "лет")
+        winery_age=get_declination_year(winery_age, "год", "года", "лет")
     )
 
     with open('index.html', 'w', encoding="utf8") as file:
